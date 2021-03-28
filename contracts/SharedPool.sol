20dd661ac1f35610079a645eebe32c29ee5e1232
masaun <masanoriuno@gmail.com>
2021-03-28 22:58:02 +0900

    Define the User struct

--- contracts/SharedPool.sol
+++ contracts/SharedPool.sol
@@ -1,11 +1,22 @@
 // SPDX-License-Identifier: MIT
 pragma solidity 0.6.11;
 
+import { SafeMath } from "@openzeppelin/contracts/math/SafeMath.sol";
+
 
 /**
  * @notice - This is a smart contract that 
  */
 contract SharedPool {
+    using SafeMath for uint
+
+    uint currentUserId;
+
+    struct User {
+        address walletAddress;
+        uint depositAmount;
+    }
+    User[] users;
 
     constructor() public {}
 
@@ -14,7 +25,21 @@ contract SharedPool {
      * @notice - Deposited-amount of this pool is used for openTrove.
      */
     function depositToSharedPool() public returns (bool) {
-        /// [Todo]: User transfer ETH into this pool contract
+        /// User transfer ETH into this pool contract
+        uint newUserId = getNextUserId();
+        currentUserId++;
+
+        User memory user = User({
+            walletAddress: msg.sender,
+            depositAmount: msg.value
+        })
+        users.push(user);
     }
+
+
+    function getNextUserId() private returns (uint _nextUserId) {
+        currentUserId.add(1);
+    }
+    
 
 }
