cb8698e3204442ebf0971579cc08ef8bf0683901
masaun <masanoriuno@gmail.com>
2021-03-28 19:31:18 +0900

    Define SharedPool.sol + Define depositToSharedPool()

---
+++ contracts/SharedPool.sol
@@ -0,0 +1,20 @@
+// SPDX-License-Identifier: MIT
+pragma solidity 0.6.11;
+
+
+/**
+ * @notice - This is a smart contract that 
+ */
+contract SharedPool {
+
+    constructor() public {}
+
+    /**
+     * @notice - Users deposit this shared-pool
+     * @notice - Deposited-amount of this pool is used for openTrove.
+     */
+    function depositToSharedPool() public returns (bool) {
+        /// [Todo]: User transfer ETH into this pool contract
+    }
+
+}
