e8dc711ba1156c2ffbf9899666d474d89e78d51c
masaun <masanoriuno@gmail.com>
2021-03-28 10:54:31 +0900

    Define SharedTrove.sol and openTroveWithMultipleUsers() method

---
+++ contracts/SharedTrove.sol
@@ -0,0 +1,13 @@
+// SPDX-License-Identifier: MIT
+pragma solidity 0.6.11;
+
+/**
+ * @notice - This is a smart contract that 
+ */
+contract SharedTrove {
+
+    constructor() public {}
+
+    function openTroveWithMultipleUsers() public returns (bool) {}
+
+}
