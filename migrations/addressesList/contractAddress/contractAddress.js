fa1fdd65a6c7b7d352deb7a050bd7c85246ed3aa
masaun <masanoriuno@gmail.com>
2021-03-28 10:06:15 +0900

    Update contract/tokenAddress.js with Liquity on Kovan

--- migrations/addressesList/contractAddress/contractAddress.js
+++ migrations/addressesList/contractAddress/contractAddress.js
@@ -1,16 +1,22 @@
 module.exports = {
     "Mainnet": {
-        "PoolTogether": {
-            "PoolWithMultipleWinnersBuilder": "0x39E2F33ff4Ad3491106B3BB15dc66EbE24e4E9C7"
-        },
-        "mStable": {
-            "mUSD": "0xe2f2a5C287993345a840Db3B0845fbC70f5935a5",
-            "mUSD SAVE (imUSD)": "0x30647a72Dc82d7Fbb1123EA74716aB8A317Eac19",
-            "Nexus": "0xAFcE80b19A8cE13DEc0739a1aaB7A028d6845Eb3"
-        }
     },
-    "Ropsten": {
-    },
-    "Rinkeby": {
-    },
+    "Kovan": {
+        "Liquity": {
+            "activePool": "0x11025B04B5b43eBeB434eD6de042906d99cb2D29",
+            "borrowerOperations": "0xaf5275eB6FD0796A543F33530cb404d587BE4d30",
+            "troveManager": "0x47a2d855fa5b501612744Cb65973273822bb9614",
+            "collSurplusPool": "0x79772BA2Ec7fa8554F193EC555394E2b9Df7faeb",
+            "communityIssuance": "0x95C27eB4635F99B36D098e508dc134F4a2a0683F",
+            "defaultPool": "0xa025B0FC7a57e5177C699E8EC829e22CaB2b35f0",
+            "hintHelpers": "0x75bAFaA9ad7DC813C2accd7A6bb6699541aBd469",
+            "lockupContractFactory": "0xfB5f94932C08982C0b052610e10CDfdE13dD19ad",
+            "lqtyStaking": "0xC771c641240f09DaCa33D4D678F4cbcb3EEDb792",
+            "priceFeed": "0x528D49210a3289C457506617fC3bf31Cc6b76107",
+            "sortedTroves": "0x737153043DE2EB8fd0CE8928EC3dba096185a833",
+            "stabilityPool": "0x4B37C0F3072604cd9540b2b71a022d4bfa22960F",
+            "gasPool": "0x0A33A76fC320347BE3ed76bFbF24Eb6DE884cc35",
+            "multiTroveGetter": "0x24EAbBd522655405E9e9e0e325912A2EdbCDcECE" 
+        }       
+    }
 }
