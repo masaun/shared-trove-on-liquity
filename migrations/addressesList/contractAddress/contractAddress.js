c8a58baf3f223866d2964eb43e2c415a81eb8219
masaun <masanoriuno@gmail.com>
2021-03-28 09:56:28 +0900

    first commit

---
+++ migrations/addressesList/contractAddress/contractAddress.js
@@ -0,0 +1,16 @@
+module.exports = {
+    "Mainnet": {
+        "PoolTogether": {
+            "PoolWithMultipleWinnersBuilder": "0x39E2F33ff4Ad3491106B3BB15dc66EbE24e4E9C7"
+        },
+        "mStable": {
+            "mUSD": "0xe2f2a5C287993345a840Db3B0845fbC70f5935a5",
+            "mUSD SAVE (imUSD)": "0x30647a72Dc82d7Fbb1123EA74716aB8A317Eac19",
+            "Nexus": "0xAFcE80b19A8cE13DEc0739a1aaB7A028d6845Eb3"
+        }
+    },
+    "Ropsten": {
+    },
+    "Rinkeby": {
+    },
+}
